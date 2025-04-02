8e53e7b933c3ad610d54cd09e1062e90737f4764
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-04-02 18:47:31 +0100

    new remap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "P", "p")
+vim.keymap.set("n", "p", "P")
