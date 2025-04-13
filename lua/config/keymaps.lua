bac2fc30db8fffe9063b8dea4ab19fc742dcf72e
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-04-13 22:45:45 +0100

    Add diagnostics keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,3 +28,7 @@ vim.keymap.set("n", "<leader>cbd", function()
 end)
 
 vim.keymap.set("n", "<leader>s", "i <esc>")
+vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
