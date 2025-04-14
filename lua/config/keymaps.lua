25c21eea3bc75e6efbd714ac5ede61f1fcc87603
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-04-14 22:19:15 +0100

    Add hoogle keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,3 +32,4 @@ vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { 
 vim.keymap.set("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>hg", "<cmd>Telescope hoogle<CR>", { noremap = true, silent = true })
