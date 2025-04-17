5a91d795b1ab352a8e45ca71d3af6a562b3d3af4
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-04-17 23:12:32 +0100

    TestCase snippet for haskell

--- lua/snippets/haskell.lua
+++ lua/snippets/haskell.lua
@@ -1,7 +1,31 @@
-require("luasnip.session.snippet_collection").clear_snippets "haskell"
+require("luasnip.session.snippet_collection").clear_snippets("haskell")
 
-local ls = require('luasnip')
+local ls = require("luasnip")
 
-local haskell_snippets = require('haskell-snippets').all
-ls.add_snippets('haskell', haskell_snippets, { key = 'haskell' })
+local haskell_snippets = require("haskell-snippets").all
+ls.add_snippets("haskell", haskell_snippets, { key = "haskell" })
 
+local s = ls.snippet
+local i = ls.insert_node
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+ls.add_snippets("haskell", {
+	s(
+		{ trig = "testcase" },
+		fmt(
+			[[
+            <> :: TestTree  
+            <> = testCase "<>"
+                $ <>
+            ]],
+			{ i(1, "test"), rep(1), i(2, "test"), i(3, "undefined") },
+			{ delimiters = "<>" }
+		)
+	),
+})
