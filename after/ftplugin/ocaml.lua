65bbe215be965023dc8d33eb7544cef5b6f42ec9
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-05-01 10:13:17 +0100

    Toggle ml/mli first iteration

--- after/ftplugin/ocaml.lua
+++ after/ftplugin/ocaml.lua
@@ -3,3 +3,27 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 
+local toggle_interface_impementation = function(cmd)
+	local client = assert(vim.lsp.get_clients()[1])
+	client:request("ocamllsp/switchImplIntf", { vim.fn.expand("%") }, function(err, res, ctx)
+		local next = next
+		local _, file_uri = next(res)
+		if file_uri == nil then
+			print("LSP didn't return anything")
+		elseif #res == 1 then
+			local file_name = vim.uri_to_fname(file_uri)
+			-- this is stupid because ocaml-lsp returns
+			-- file path starting with /
+			vim.cmd(cmd .. " " .. string.sub(file_name, 2))
+		else
+			print("LSP returned multiple results")
+		end
+	end)
+end
+
+vim.keymap.set("n", "<localleader>i", function()
+	toggle_interface_impementation("e")
+end)
+vim.keymap.set("n", "<localleader>v", function()
+	toggle_interface_impementation("vs")
+end)
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,9 @@
 return {
 	"tpope/vim-fugitive",
 	event = "BufWinEnter",
+	config = function()
+		vim.keymap.set("n", "<F12>", "<cmd>Git push<CR>", { noremap = true })
+		-- <S-F12>
+		vim.keymap.set("n", "<F24>", "<cmd>Git push -f<CR>", { noremap = true })
+	end,
 }
