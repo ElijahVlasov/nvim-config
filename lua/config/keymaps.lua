73ae3fb499d92b6d113b92c76b8d7b2f39f85d8f
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-05-08 14:46:23 +0100

    Fix keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,49 +6,39 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
 
-vim.keymap.set("n", "<S-h>", "<cmd>bprev<CR>")
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>")
+vim.keymap.set("n", "H", "<cmd>bprev<CR>")
+vim.keymap.set("n", "L", "<cmd>bnext<CR>")
 
 vim.keymap.set("n", "P", "p")
 vim.keymap.set("n", "p", "P")
 
-vim.keymap.set("n", "<A-i>", "<C-a>")
-
-vim.keymap.set("n", "<localleader>j", function()
-	local r, c = unpack(vim.api.nvim_win_get_cursor(0))
-	vim.cmd([[norm O]])
-	vim.api.nvim_win_set_cursor(0, { r + 1, c })
-end)
-vim.keymap.set("n", "<localleader>k", function()
-	local c = vim.api.nvim_win_get_cursor(0)
-	vim.cmd([[norm o]])
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-e>", true, false, true), "n", false)
-	vim.api.nvim_win_set_cursor(0, c)
-end)
+vim.keymap.set("n", "<A-i>", "<C-a>", { desc = "Avoiding clash with the tmux" })
 
 vim.keymap.set("n", "<leader>puc", function()
 	vim.cmd([[colorscheme catppuccin-macchiato]])
-end)
+end, { desc = "Turn on catppuccin macchiato" })
 vim.keymap.set("n", "<leader>mmm", function()
 	vim.cmd([[colorscheme fakedonalds]])
-end)
+end, { desc = "Turn on fakedonalds" })
 vim.keymap.set("n", "<leader>cbd", function()
 	vim.cmd([[colorscheme carbonized-light]])
-end)
+end, { desc = "Turn on carbonized light" })
 
 vim.keymap.set("n", "<leader>s", "i <esc>")
 vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>hg", "<cmd>Telescope hoogle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<localleader>hg", "<cmd>Telescope hoogle<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<localleader>pl", function()
 	local hash = vim.api.nvim_exec2("Git rev-parse HEAD", { output = true }).output
 	local url = vim.api.nvim_exec2("Git remote get-url origin", { output = true }).output
 	local full_name = vim.api.nvim_exec2("Git ls-files --full-name %", { output = true }).output
-	local _, line = unpack(vim.api.nvim_win_get_cursor(0))
+	local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
 	if string.sub(url, 1, string.len("https://")) == "https://" then
 		url = url:gsub("%.git", "")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,48 +1,64 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 100,
+				tabline = 100,
+				winbar = 100,
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = {
+				"branch",
+				"diff",
+				{ "diagnostics", symbols = { error = " ", warn = "", info = " ", hint = " " } },
+			},
+			lualine_c = { "filename", "lsp_status" },
+			lualine_x = {
+				{
+					function()
+						for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+							if vim.api.nvim_get_option_value("modified", { buf = buf }) then
+								return "Unsaved bufs 🥷🏼" -- any message or icon
+							end
+						end
+						return ""
+					end,
 				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				always_show_tabline = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 100,
-					tabline = 100,
-					winbar = 100,
-				},
 			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename", "lsp_status" },
-				lualine_x = {},
-				lualine_y = { "encoding", "fileformat", "filetype" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		})
+			lualine_y = { "encoding" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
+	config = function(_, opts)
+		require("lualine").setup(opts)
 		vim.opt_global.showmode = false
 	end,
 }
