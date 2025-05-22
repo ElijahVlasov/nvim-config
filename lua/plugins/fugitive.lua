e0f58a640ce064e9604221659cdf9e1091ca101c
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-05-22 17:49:19 +0100

    New git keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,12 @@ return {
 	"tpope/vim-fugitive",
 	event = "BufWinEnter",
 	config = function()
-		vim.keymap.set("n", "<F12>", "<cmd>Git push<CR>", { noremap = true })
+		vim.keymap.set("n", "<F10>", "<cmd>Git add .<CR>", { noremap = true, desc = "git add ." })
+		vim.keymap.set("n", "<F22>", "<cmd>Git add .<CR>", { noremap = true, desc = "git add ." })
+		vim.keymap.set("n", "<F11>", "<cmd>Git commit<CR>", { noremap = true, desc = "git commit" })
+		vim.keymap.set("n", "<F23>", "<cmd>Git commit --amend<CR>", { noremap = true, desc = "git commit --amend" })
+		vim.keymap.set("n", "<F12>", "<cmd>Git push<CR>", { noremap = true, desc = "git push" })
 		-- <S-F12>
-		vim.keymap.set("n", "<F24>", "<cmd>Git push -f<CR>", { noremap = true })
+		vim.keymap.set("n", "<F24>", "<cmd>Git push -f<CR>", { noremap = true, desc = "git push -f" })
 	end,
 }
