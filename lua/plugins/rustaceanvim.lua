6e7515e8f2bce5e7d71de8a00fad1bf16305ba7f
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-11-13 12:48:58 +0000

    Some rust stuff

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,4 +2,12 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^6", -- Recommended
 	lazy = false, -- This plugin is already lazy
+	config = function()
+		vim.keymap.set("n", "<localleader>c", function()
+			vim.cmd.RustLsp("openCargo")
+		end)
+		vim.keymap.set("n", "<localleader>r", function()
+			vim.cmd.RustAnalyzer("restart")
+		end)
+	end,
 }
